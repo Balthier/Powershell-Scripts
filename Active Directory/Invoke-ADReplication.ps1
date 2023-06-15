fad45b6e3dfba11c0ee30d22b896b355fce68f09
Balthier Lionheart <raptors.ftw@gmail.com>
2023-06-15 12:23:34 +0100

    Added KCC checks

--- Active Directory/Invoke-ADReplication.ps1
+++ Active Directory/Invoke-ADReplication.ps1
@@ -1,11 +1,29 @@
 <#
 .Synopsis
-Starts an AD replication in both directions, for all Domain Controllers in the Forest
+Starts a KCC and runs AD replication in both directions, for all Domain Controllers in the Forest
 #>
 
-$StartTime = Get-Date
-$AllDomains = (Get-ADForest).Domains
 
+$StartTime = Get-Date
+$WaitTime = 300
+
+$Sites = (Get-ADForest).Sites
+ForEach ($Site in $Sites) {
+    $DCList = Get-ADDomainController -Filter { site -eq $site }
+    if ($DCList) {
+        Write-Host "DCs found in $Site. Initiating KCC."
+        repadmin /kcc site:$Site
+        Clear-Variable DCList
+    }
+    else {
+        Write-Host "No DCs found in $Site. Skipping KCC."
+    }
+}
+
+Write-Host "KCC initiated against all sites. Waiting for $WaitTime seconds, to allow KCC to complete"
+Start-Sleep -Seconds $WaitTime
+
+$AllDomains = (Get-ADForest).Domains
 ForEach ($Domain in $AllDomains) {
     Write-Host $Domain": Getting Domain Controllers"
     $DCs = Get-ADDomainController -Filter * -Server $Domain
@@ -18,6 +36,7 @@ ForEach ($Domain in $AllDomains) {
         }
     }
 }
+
 $Runtime = (Get-Date) - $StartTime
 $Runtime = $Runtime -f ("HH:mm:ss")
 Write-Host "`n[$(Get-Date -DisplayHint Time)] Total Runtime:"$Runtime"`n"
